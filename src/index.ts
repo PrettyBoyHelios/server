fb89d73e7b93e88f6e5cec4c182696113de55ef8
Luis Correa <helios@polispay.org>
2022-07-28 00:26:57 -0500

    added nodemon for dev & validations for json body

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { body, validationResult } from "express-validator";
 
 const app = express();
 const port = 8080; // default port to listen
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // define a route handler for the default home page
 app.get( "/", ( req: any, res: any ) => {
@@ -9,8 +14,25 @@ app.get( "/", ( req: any, res: any ) => {
 } );
 
 
-app.post( "/register/", ( req: any, res: any ) => {
-    res.send( "Registering" );
+app.post( "/register/",
+body('email').isEmail().normalizeEmail(),
+body('phone').isMobilePhone("es-MX"),
+body('password').isLength({
+    max: 64,
+    min: 8,
+}),
+( req: any, res: any ) => {
+    const errors = validationResult(req);
+    console.log("validation errors: ", errors);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            success: false,
+            errors: errors.array()
+        })
+    }
+    console.log(req.body);
+    // TODO Add mongo db store logic
+    res.send( "Registering!!" );
 } );
 
 
